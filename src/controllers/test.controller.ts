667a1799b5154078db3afef7a96403ffbcf1be18
Kasemsan <kasemsan.cho@gmail.com>
2023-01-24 18:54:10 +0700

    init

--- src/controllers/test.controller.ts
+++ src/controllers/test.controller.ts
@@ -1,4 +1,5 @@
 import { RequestHandler } from "express";
+const prisma = require("../connection");
 
 export const test: RequestHandler = (req, res, next) => {
   res.json({
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
-import { Prisma, PrismaClient } from "@prisma/client";
 import express from "express";
 import morgan from "morgan";
 
-const prisma = new PrismaClient();
 const app = express();
 
 const indexRouter = require("./routes/index");
@@ -10,7 +8,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use("/", indexRouter);
 
-const server = app.listen(5500, () =>
+app.listen(5500, () =>
   console.log(`
 🚀 Server ready at: http://localhost:3000
 ⭐️ See sample requests: http://pris.ly/e/ts/rest-express#3-using-the-rest-api`),
