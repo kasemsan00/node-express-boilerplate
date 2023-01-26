3d1071191a9993cf08dc09cd2ec1218bba37a1db
Kasemsan <kasemsan.cho@gmail.com>
2023-01-27 00:41:58 +0700

    init

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,17 @@ app.use(express.json());
 app.use(cors());
 app.use("/", indexRouter);
 
-app.listen(process.env.PORT, () => {
-  logger.info("Server is running on port " + process.env.PORT);
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import typeDefs from "./graphql/schema";
+import resolvers from "./graphql/resolvers";
+import { loggers } from "winston";
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+startStandaloneServer(server, { listen: { port: 4000 } }).then(({ url }) => {
+  logger.info(`Server ready at: ${url}`);
 });
