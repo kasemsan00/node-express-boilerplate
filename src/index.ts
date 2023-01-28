6543c1d73e3faf1463e18bccae56181b9a960fc3
Kasemsan <kasemsan.cho@gmail.com>
2023-01-28 09:49:09 +0700

    init

--- src/index.ts
+++ src/index.ts
@@ -5,24 +5,38 @@ import morgan from "morgan";
 import cors = require("cors");
 import logger from "./config/logger";
 import indexRouter from "./routes/index";
+import bodyParser from "body-parser";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import http from "http";
+import typeDefs from "./graphql/schema";
+import resolvers from "./graphql/resolvers";
+
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
-app.use("/", indexRouter);
-
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
-import typeDefs from "./graphql/schema";
-import resolvers from "./graphql/resolvers";
-import { loggers } from "winston";
-
+const httpServer = http.createServer(app);
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-startStandaloneServer(server, { listen: { port: 4000 } }).then(({ url }) => {
-  logger.info(`Server ready at: ${url}`);
-});
+(async () => {
+  await server.start();
+  app.use("/", indexRouter);
+  app.use(
+    "/graphql",
+    cors(),
+    bodyParser.json({ limit: "150mb" }),
+    expressMiddleware(server, {
+      context: async ({ req }) => ({ token: req.headers.token }),
+    }),
+  );
+  httpServer.listen({ port: 4000 }, () => {
+    logger.info(`🚀 Server ready at http://localhost:4000/`);
+  });
+})();
